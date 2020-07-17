cd12cd0cf071ac797bd51006e452eaef07bd41a5
Oz <oz.amram@gmail.com>
2020-07-18 00:02:51 +0200

    Added a holdout set for MC sampler. Passed a small test

--- H5_sampler/Sampler.py
+++ H5_sampler/Sampler.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np 
 
 class Sampler():
-    def __init__(self, filename, xsec, lumi, isSignal = False):
+    def __init__(self, filename, xsec, lumi, holdout_frac = 0., isSignal = False):
         self.filename = filename
         with h5py.File(self.filename, "r") as h5_file:
             self.eff_xsec = xsec * h5_file['preselection_eff'][0]
@@ -11,11 +11,20 @@ class Sampler():
         self.nSample = int(lumi * self.eff_xsec)
         if(isSignal): self.nSample = int(lumi * xsec)
         print("Will sample %i events from file %s" %(self.nSample, self.filename))
-        if(self.nSample > self.nEvents):
-            print("Warning not enough unique events in file %s to match cross section! Need to sample %i events but only have %i saved \n" 
-                    %(self.filename, self.nSample, self.nEvents))
-        #sample with replacement
-        self.sample_idxs = np.random.choice(self.nEvents, self.nSample)
+        self.n_to_sample_from = self.nEvents
+        self.nHoldOut = 0
+        if(holdout_frac > 0.):
+            self.n_to_sample_from = int(self.nEvents * (1. - holdout_frac) )
+            self.nHoldOut = self.nEvents - self.n_to_sample_from
+        if(self.nSample > self.n_to_sample_from):
+            print("Warning not enough unique events in file %s to match cross section! Need to sample %i events but only have %i saved (holdout frac =%.02f) \n" 
+                    %(self.filename, self.nSample, self.n_to_sample_from, holdout_frac))
+            #sample with replacement
+            self.sample_idxs = np.random.choice(self.n_to_sample_from, self.nSample)
+
+        else: #no replacement
+            self.sample_idxs = np.random.choice(self.n_to_sample_from, self.nSample, replace = False)
+
 
     def sample(self, key):
         if(key not in self.keys):
@@ -27,9 +36,21 @@ class Sampler():
             out = h5_file[key][()][self.sample_idxs]
             return out
 
+    def holdout(self, key):
+        if(key not in self.keys):
+            print("Error! Key %s not in list of keys for file %s. Keys are: \n" % (key, self.filename))
+            print(self.keys)
+            exit(1)
+        if(self.nHoldOut == 0): return []
+        with h5py.File(self.filename, "r") as h5_file:
+            out = h5_file[key][self.n_to_sample_from:]
+            return out
+
+
 class BlackBox():
     def __init__(self, samplers, keys = []):
         self.data = dict()
+        self.holdout = dict()
         if(len(keys) == 0):
             #empty list means keep everything
             keys = (samplers[0]).keys
@@ -40,6 +61,10 @@ class BlackBox():
         for sample in samplers:
             self.nEvents += sample.nSample
 
+        self.nHoldOut = 0
+        for sample in samplers:
+            self.nHoldOut += sample.nHoldOut
+
         print("Creating a blackbox with %i events" % self.nEvents)
         shuffle_order = np.arange(self.nEvents)
         np.random.shuffle(shuffle_order)
@@ -47,11 +72,23 @@ class BlackBox():
         for key in keys:
             print("Getting data for key %s " % key)
             for i,sam in enumerate(samplers):
-                if(i==0): self.data[key] = sam.sample(key)
-                else: self.data[key] = np.append(self.data[key], sam.sample(key), axis = 0)
+                if(i==0): 
+                    self.data[key] = sam.sample(key)
+                    if(self.nHoldOut > 0):
+                        self.holdout[key] = sam.holdout(key)
 
-        #Shuffle the order
+                else: 
+                    self.data[key] = np.append(self.data[key], sam.sample(key), axis = 0)
+                    if(self.nHoldOut > 0):
+                        self.holdout[key] = np.append(self.holdout[key], sam.holdout(key), axis = 0)
+
+            #Shuffle the order
             self.data[key] = self.data[key][shuffle_order]
+            if(self.nHoldOut > 0):
+                holdout_order = np.arange(self.nHoldOut)
+                np.random.shuffle(holdout_order)
+                self.holdout[key] = self.holdout[key][holdout_order]
+
 
     def __getitem__(self, key):
        if(key not in self.keys):
@@ -67,6 +104,21 @@ class BlackBox():
                 shape = list(self.data[key].shape)
                 shape[0] = None
                 f.create_dataset(key, data = self.data[key], chunks = True, maxshape = shape,   compression = 'gzip')
+
+    def writeHoldOut(self, filename):
+        if(self.nHoldOut <= 0):
+            print("No holdout events saved!")
+            return
+        print("Writing out file with holdouts to %s" % filename)
+        with h5py.File(filename, "w") as f:
+            for key in self.keys:
+                shape = list(self.holdout[key].shape)
+                shape[0] = None
+                f.create_dataset(key, data = self.holdout[key], chunks = True, maxshape = shape,   compression = 'gzip')
+
+
+
+
 
 
 
--- H5_sampler/sampler_test.py
+++ H5_sampler/sampler_test.py
@@ -3,16 +3,34 @@ from Sampler import *
 np.random.seed(123)
 
 pbTofb = 1000.
-qcd1 = Sampler("../H5_maker/test_files/QCD_HT1000to1500_test.h5", pbTofb * 1088., 1.)
-qcd2 = Sampler("../H5_maker/test_files/QCD_HT1500to2000_test.h5", pbTofb * 99.11, 1.)
-qcd3 = Sampler("../H5_maker/test_files/QCD_HT2000toInf_test.h5", pbTofb * 20.23, 1.)
-sig = Sampler("../H5_maker/test_files/WprimeToWZToWhadZhad_narrow_M-3500_TuneCP5_13TeV-madgraph_test.h5", 30000., 1.)
+holdout_frac = 0.1
+qcd1 = Sampler("../H5_maker/test_files/QCD_HT1000to1500_test.h5", pbTofb * 1088., 1., holdout_frac = holdout_frac)
+qcd2 = Sampler("../H5_maker/test_files/QCD_HT1500to2000_test.h5", pbTofb * 99.11, 1., holdout_frac = holdout_frac)
+qcd3 = Sampler("../H5_maker/test_files/QCD_HT2000toInf_test.h5", pbTofb * 20.23, 1., holdout_frac = holdout_frac)
+sig = Sampler("../H5_maker/test_files/WprimeToWZToWhadZhad_narrow_M-3500_TuneCP5_13TeV-madgraph_test.h5", 30000., 1., holdout_frac = holdout_frac)
 
-ws = [qcd1, qcd2, qcd3, sig]
-#keys= ['event_info', 'jet_kinematics', 'truth_label', 'jet1_extraInfo', 'jet1_PFCands']
-keys= []
+#ws = [qcd1, qcd2, qcd3, sig]
+ws = [qcd1]
+keys= ['event_info', 'jet_kinematics', 'truth_label', 'jet1_extraInfo', 'jet1_PFCands']
+#keys= []
 BB = BlackBox(ws, keys)
 #print(BB['truth_label'].shape)
 #print(BB['jet_kinematics'].shape)
 
-BB.writeOut('BB_test_Wprime.h5')
+BB.writeOut('BB_test_Wprime2.h5')
+BB.writeHoldOut('BB_test_Wprime_holdout.h5')
+
+f1 = h5py.File("BB_test_Wprime2.h5")
+f_out = h5py.File("BB_test_Wprime_holdout.h5")
+f_orig = h5py.File("../H5_maker/test_files/QCD_HT1000to1500_test.h5")
+
+e1 = f1['event_info'][:,0]
+e_out = f_out['event_info'][:,0]
+e_orig = f_orig['event_info'][:,0]
+
+print(e_orig[99000], e_orig[0])
+print(np.where(e_out == e_orig[99000]))
+print(np.where(e_out == e_orig[0]))
+print(np.where(e1 == e_orig[99000]))
+print(np.where(e1 == e_orig[0]))
+
